eba8f10138fc75422259516e3fcfb62617c8a827
Chenghao Lyu <chenghao@cs.umass.edu>
2023-12-30 17:57:57 +0100

    update (#2)

--- udao_trace_examples/spark_trace_collector.py
+++ udao_trace_examples/spark_trace_collector.py
@@ -4,52 +4,32 @@ from udao_trace.collector.SparkCollector import SparkCollector
 from udao_trace.utils import BenchmarkType, ClusterName
 
 if __name__ == "__main__":
+    # fmt: off
     parser = argparse.ArgumentParser(description="Spark Trace Collection Script")
-    parser.add_argument(
-        "--knob_meta_file",
-        type=str,
-        default="assets/spark_configuration_aqe_on.json",
-        help="Path to the knob metadata file",
-    )
-    parser.add_argument(
-        "--benchmark_type",
-        type=str,
-        default="TPCH",
-        help="Type of benchmark (e.g., TPCH)",
-    )
-    parser.add_argument(
-        "--scale_factor", type=int, default=100, help="Scale factor of the benchmark"
-    )
-    parser.add_argument(
-        "--cluster_name", type=str, default="HEX1", help="Name of the cluster"
-    )
-    parser.add_argument(
-        "--parametric_bash_file",
-        type=str,
-        default="assets/run_spark_collect.sh",
-        help="Path to the parametric bash file",
-    )
+    parser.add_argument("--knob_meta_file", type=str,
+                        default="assets/spark_configuration_aqe_on.json",
+                        help="Path to the knob metadata file")
+    parser.add_argument("--benchmark_type", type=str, default="TPCH",
+                        help="Type of benchmark (e.g., TPCH)")
+    parser.add_argument("--scale_factor", type=int, default=100,
+                        help="Scale factor of the benchmark")
+    parser.add_argument("--cluster_name", type=str, default="HEX1",
+                        help="Name of the cluster")
+    parser.add_argument("--parametric_bash_file", type=str,
+                        default="assets/run_spark_collect.sh",
+                        help="Path to the parametric bash file")
     parser.add_argument("--debug", action="store_true", help="Enable debug mode")
-    parser.add_argument(
-        "--default", action="store_true", help="Run default configurations"
-    )
-    parser.add_argument(
-        "--n_data_per_template",
-        type=int,
-        default=10,
-        help="Number of data points per template, 2273 for TPCH, 490 for TPCDS",
-    )
-    parser.add_argument(
-        "--n_processes",
-        type=int,
-        default=16,
-        help="Number of processes for parallel execution",
-    )
-    parser.add_argument(
-        "--cluster_cores", type=int, default=150, help="Total available cluster cores"
-    )
-    parser.add_argument("--seed", type=int, default=42, help="Seed for randomization")
-
+    parser.add_argument("--default", action="store_true",
+                        help="Run default configurations")
+    parser.add_argument("--n_data_per_template", type=int, default=10,
+                        help="Data points per template, 2273 for TPCH, 490 for TPCDS")
+    parser.add_argument("--n_processes", type=int, default=16,
+                        help="Number of processes for parallel execution")
+    parser.add_argument("--cluster_cores", type=int, default=150,
+                        help="Total available cluster cores")
+    parser.add_argument("--seed", type=int, default=42,
+                        help="Seed for randomization")
+    # fmt: on
     args = parser.parse_args()
 
     spark_collector = SparkCollector(
