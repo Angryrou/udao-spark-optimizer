738858db5384edf965a47e4ec149282dd4035e06
angryrou <chenghao@cs.umass.edu>
2023-12-30 17:57:21 +0100

    update

--- udao_trace_examples/spark_trace_parser.py
+++ udao_trace_examples/spark_trace_parser.py
@@ -18,28 +18,19 @@ from udao_trace.utils import BenchmarkType
 from udao_trace.utils.logging import _get_logger
 
 if __name__ == "__main__":
+    # fmt: off
     parser = argparse.ArgumentParser(description="Spark Trace Collection Script")
-    parser.add_argument(
-        "--header", type=str, default="./spark_collector/tpch100/lhs_22x2273"
-    )
-    parser.add_argument(
-        "--benchmark_type",
-        type=str,
-        default="TPCH",
-        help="Type of benchmark (e.g., TPCH)",
-    )
-    parser.add_argument(
-        "--scale_factor", type=int, default=100, help="Scale factor of the benchmark"
-    )
-    parser.add_argument(
-        "--upto", type=int, default=10, help="Name of the traces for each template"
-    )
-    parser.add_argument(
-        "--n_processes",
-        type=int,
-        default=1,
-        help="number of parallel processors for parsing traces",
-    )
+    parser.add_argument("--header", type=str,
+                        default="./spark_collector/tpch100/lhs_22x2273")
+    parser.add_argument("--benchmark_type", type=str, default="TPCH",
+                        help="Type of benchmark (e.g., TPCH)",)
+    parser.add_argument("--scale_factor", type=int, default=100,
+                        help="Scale factor of the benchmark")
+    parser.add_argument("--upto", type=int, default=10,
+                        help="Name of the traces for each template")
+    parser.add_argument("--n_processes", type=int, default=1,
+                        help="number of parallel processors for parsing traces",)
+    # fmt: on
 
     args = parser.parse_args()
 
