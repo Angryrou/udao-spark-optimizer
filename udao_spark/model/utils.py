721fbf3cf1344be8af9a15e66cfd02dcea24d4bb
Chenghao Lyu <chenghao@cs.umass.edu>
2024-02-06 12:35:36 +0100

    update

--- udao_spark/model/utils.py
+++ udao_spark/model/utils.py
@@ -23,7 +23,7 @@ from udao.utils.logging import logger
 
 from udao_trace.utils import JsonHandler
 
-from ..utils.params import UdaoParams
+from ..utils.params import QType, UdaoParams
 
 
 @dataclass
@@ -309,6 +309,26 @@ def get_graph_ckp_info(weights_path: str) -> Tuple[str, str, str, str]:
     data_processor_path = "/".join(splits[:4] + ["data_processor.pkl"])
     model_params_path = "/".join(splits[:5] + ["model_struct_params.json"])
     return ag_prefix, model_sign, model_params_path, data_processor_path
+
+
+LAT_MIN_MAP = {
+    "tpch": {
+        "q_compile": 3.073,
+        "q_all": 0.076,
+        "qs_lqp_compile": 0.0001625,
+        "qs_lqp_runtime": 0.0001625,
+        "qs_pqp_runtime": 0.0001625,
+    },
+}
+
+
+def calibrate_negative_predictions(
+    y_pred: np.ndarray, bm: str, q_type: Optional[QType] = None
+) -> np.ndarray:
+    if q_type is None:
+        return np.clip(y_pred, a_min=0, a_max=None)
+    else:
+        return np.clip(y_pred, a_min=LAT_MIN_MAP[bm][q_type], a_max=None)
 
 
 def local_wmape(y_true: np.ndarray, y_pred: np.ndarray) -> float:
