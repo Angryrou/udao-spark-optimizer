217fb45aca29abb01250cbb55d8101efeefdf88b
Chenghao Lyu <chenghao@cs.umass.edu>
2024-02-15 19:54:49 +0100

    retrain model details

--- udao_spark/utils/params.py
+++ udao_spark/utils/params.py
@@ -147,11 +147,11 @@ def get_ag_parameters() -> ArgumentParser:
     parser = get_base_parser()
     # fmt: off
     parser.add_argument("--hp_choice", type=str, default="default",
-                        choices=["default", "tuned-0202"])
+                        choices=["default", "tuned-0202", "tuned-0215"])
     parser.add_argument("--graph_choice", type=str, default="gtn",
                         choices=["avg", "gtn"])
-    parser.add_argument("--ag_sign", type=str, default="ag_default_hp",
-                        choices=["ag_default_hp"])
+    parser.add_argument("--ag_sign", type=str, default="ag",
+                        choices=["ag"])
     parser.add_argument("--num_gpus", type=int, default=2,)
     parser.add_argument("--infer_limit", type=float, default=None,
                         help="Inference limit, e.g., 1e-5")
