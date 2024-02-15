ce64ab2ba8bf45678882cbfdb383287d2c7abb63
QiFAN-lix <zdh1204fanqiqi@163.com>
2024-02-15 08:01:07 +0100

    update div_moo

--- udao_spark/optimizer/hierarchical_optimizer.py
+++ udao_spark/optimizer/hierarchical_optimizer.py
@@ -59,6 +59,7 @@ class HierarchicalOptimizer(BaseOptimizer):
         non_decision_df: pd.DataFrame,
         sampled_theta: np.ndarray,
         model_name: str,
+        cost_choice: str = "ana_cost_w_io",
     ) -> np.ndarray:
         start_time_ns = time.perf_counter_ns()
         objs = self.ag_ms.predict_with_ag(
@@ -83,7 +84,7 @@ class HierarchicalOptimizer(BaseOptimizer):
         )
 
         ana_latency = objs["ana_latency_s"]
-        ana_cost_w_io = objs_dict["ana_cost_w_io"]
+        ana_cost_w_io = objs_dict[cost_choice]
 
         return np.vstack((ana_latency, ana_cost_w_io)).T
 
@@ -100,6 +101,7 @@ class HierarchicalOptimizer(BaseOptimizer):
         param1: int = -1,
         param2: int = -1,
         time_limit: int = -1,
+        is_oracle: bool = False,
     ) -> Tuple[Optional[np.ndarray], Optional[np.ndarray]]:
         non_decision_df = self.extract_non_decision_df(non_decision_input)
         (
@@ -161,6 +163,8 @@ class HierarchicalOptimizer(BaseOptimizer):
             )
 
         elif "div_and_conq_moo" in algo:
+            # n_c_samples: param1
+            # n_p_samples: param2
             objs, conf = self._div_and_conq_moo(
                 len_theta_per_qs,
                 graph_embeddings,
@@ -174,6 +178,9 @@ class HierarchicalOptimizer(BaseOptimizer):
                 query_id,
                 save_data,
                 sample_mode,
+                param1,
+                param2,
+                is_oracle,
             )
 
         elif algo == "evo":
@@ -489,6 +496,9 @@ class HierarchicalOptimizer(BaseOptimizer):
         query_id: Optional[str],
         save_data: bool,
         sample_mode: Optional[str],
+        n_c_samples: int,
+        n_p_samples: int,
+        is_oracle: bool,
     ) -> Tuple[Optional[np.ndarray], Optional[np.ndarray]]:
         start = time.time()
         theta_c: Union[th.Tensor, np.ndarray]
@@ -511,10 +521,16 @@ class HierarchicalOptimizer(BaseOptimizer):
 
         if sample_mode == "random":
             theta_c_samples = self.sample_theta_x(
-                10, "c", seed if seed is not None else None, normalize=normalize
+                n_c_samples,
+                "c",
+                seed if seed is not None else None,
+                normalize=normalize,
             )
             theta_p_samples = self.sample_theta_x(
-                10, "p", seed + 1 if seed is not None else None, normalize=normalize
+                n_p_samples,
+                "p",
+                seed + 1 if seed is not None else None,
+                normalize=normalize,
             )
             if use_ag:
                 theta_c = theta_c_samples
@@ -523,27 +539,139 @@ class HierarchicalOptimizer(BaseOptimizer):
                 theta_c = th.tensor(theta_c_samples, dtype=th.float32)
                 theta_p = th.tensor(theta_p_samples, dtype=th.float32)
         elif sample_mode == "grid":
-            c_grids = [
-                [1, 5],
-                [1, 4],
-                [4, 16],
-                [1, 4],
-                [0, 5],
-                [0, 1],
-                [0, 1],
-                [50, 75],
-            ]
-            p_grids = [
-                [0, 5],
-                [1, 6],
-                [0, 32],
-                [0, 32],
-                [2, 50],
-                [0, 4],
-                [20, 80],
-                [0, 4],
-                [0, 4],
-            ]
+            if n_c_samples == 256:
+                c_grids = [
+                    [1, 5],
+                    [1, 4],
+                    [4, 16],
+                    [1, 4],
+                    [0, 5],
+                    [0, 1],
+                    [0, 1],
+                    [50, 75],
+                ]
+            elif n_c_samples == 128:
+                c_grids = [
+                    [1, 5],
+                    [1, 4],
+                    [4, 16],
+                    [1, 4],
+                    [0, 5],
+                    [0, 1],
+                    [1],
+                    [50, 75],
+                ]
+
+            elif n_c_samples == 64:
+                c_grids = [
+                    [1, 5],
+                    [1, 4],
+                    [4, 16],
+                    [1, 4],
+                    [0, 5],
+                    [0],
+                    [1],
+                    [50, 75],
+                ]
+            elif n_c_samples == 32:
+                c_grids = [
+                    [1, 5],
+                    [1, 4],
+                    [4, 16],
+                    [1, 4],
+                    [5],
+                    [0],
+                    [1],
+                    [50, 75],
+                ]
+            elif n_c_samples == 16:
+                c_grids = [
+                    [1, 5],
+                    [1, 4],
+                    [4, 16],
+                    [1, 4],
+                    [5],
+                    [0],
+                    [1],
+                    [50],
+                ]
+            else:
+                raise Exception(f"n_c_samples {n_c_samples} is not supported!")
+
+            if n_p_samples == 512:
+                p_grids = [
+                    [0, 5],
+                    [1, 6],
+                    [0, 32],
+                    [0, 32],
+                    [2, 50],
+                    [0, 4],
+                    [20, 80],
+                    [0, 4],
+                    [0, 4],
+                ]
+            elif n_p_samples == 256:
+                p_grids = [
+                    [0],
+                    [1, 6],
+                    [0, 32],
+                    [0, 32],
+                    [2, 50],
+                    [0, 4],
+                    [20, 80],
+                    [0, 4],
+                    [0, 4],
+                ]
+            elif n_p_samples == 128:
+                p_grids = [
+                    [0],
+                    [1, 6],
+                    [0, 32],
+                    [0, 32],
+                    [2, 50],
+                    [0, 4],
+                    [20, 80],
+                    [0],
+                    [0, 4],
+                ]
+            elif n_p_samples == 64:
+                p_grids = [
+                    [0],
+                    [1],
+                    [0, 32],
+                    [0, 32],
+                    [2, 50],
+                    [0, 4],
+                    [20, 80],
+                    [0],
+                    [0, 4],
+                ]
+            elif n_p_samples == 32:
+                p_grids = [
+                    [0],
+                    [1],
+                    [0, 32],
+                    [0, 32],
+                    [2, 50],
+                    [0],
+                    [20, 80],
+                    [0],
+                    [0, 4],
+                ]
+            elif n_p_samples == 16:
+                p_grids = [
+                    [0],
+                    [1],
+                    [0, 32],
+                    [0, 32],
+                    [2, 50],
+                    [0],
+                    [20, 80],
+                    [0],
+                    [0],
+                ]
+            else:
+                raise Exception(f"n_p_samples {n_p_samples} is not supported!")
 
             if use_ag:
                 theta_c = np.array([list(i) for i in itertools.product(*c_grids)])
@@ -555,8 +683,6 @@ class HierarchicalOptimizer(BaseOptimizer):
                 theta_p_samples = np.array(
                     [list(i) for i in itertools.product(*p_grids)]
                 )
-                # s_samples = np.array([list(i)
-                # for i in itertools.product(*s_grids)])
                 c_samples_norm = (theta_c_samples - self.theta_minmax["c"][0]) / (
                     self.theta_minmax["c"][1] - self.theta_minmax["c"][0]
                 )
@@ -565,6 +691,9 @@ class HierarchicalOptimizer(BaseOptimizer):
                     self.theta_minmax["p"][1] - self.theta_minmax["p"][0]
                 )
                 theta_p = th.tensor(p_samples_norm, dtype=th.float32)
+
+            n_c_samples = theta_c.shape[0]
+            n_p_samples = theta_p.shape[0]
 
         else:
             raise Exception(
@@ -603,8 +732,9 @@ class HierarchicalOptimizer(BaseOptimizer):
                 verbose=True,
             ),
             seed=0,
+            sample_funcs=self.sample_theta_x,
         )
-        po_objs, po_conf = div_moo.solve()
+        po_objs, po_conf, model_infer_info = div_moo.solve()
         time_cost = time.time() - start
         print(f"query id is {query_id}")
         print(f"FUNCTION: time cost of div_and_conq_moo is: {time_cost}")
@@ -620,22 +750,23 @@ class HierarchicalOptimizer(BaseOptimizer):
                 -1, len_theta_per_qs
             )
             data_path = (
-                f"./output/test/latest_model_{self.device.type}/ag/{algo}/time_-1/"
-                f"query_{query_id}_n_{n_stages}/{sample_mode}/use_ag_{use_ag}/{dag_opt_algo}"
+                f"./output/test/drop_pygmo/latest_model_{self.device.type}/ag/oracle_{is_oracle}/{algo}/{n_c_samples}_{n_p_samples}/time_-1/"
+                f"query_{query_id}_n_{n_stages}/{sample_mode}/{dag_opt_algo}"
             )
         else:
             conf_raw = self.sc.construct_configuration_from_norm(conf_qs0).reshape(
                 -1, len_theta_per_qs
             )
             data_path = (
-                f"./output/test/latest_model_{self.device.type}/mlp/{algo}/time_-1/"
-                f"query_{query_id}_n_{n_stages}/{sample_mode}/use_ag_{use_ag}/{dag_opt_algo}"
+                f"./output/test/drop_pygmo/latest_model_{self.device.type}/mlp/oracle_{is_oracle}/{algo}/{n_c_samples}_{n_p_samples}/time_-1/"
+                f"query_{query_id}_n_{n_stages}/{sample_mode}/{dag_opt_algo}"
             )
 
         if save_data:
             save_results(data_path, po_objs, mode="F")
             save_results(data_path, conf_raw, mode="Theta")
             save_results(data_path, np.array([time_cost]), mode="time")
+            save_results(data_path, model_infer_info, mode="model_infer_info")
 
         # add WUN
         objs, conf = weighted_utopia_nearest_impl(po_objs, conf_raw)
