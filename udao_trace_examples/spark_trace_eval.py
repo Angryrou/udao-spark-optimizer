4fec6562515ab33d965c11587ea7df92ab9e6b3f
Chenghao Lyu <chenghao@cs.umass.edu>
2024-02-22 17:03:21 +0100

    eval_runtime_metrics (#46)
    * eval_runtime_metrics
    
    * minor fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix the pattern
    
    * fix the pattern
    
    * support parsing default
    
    * support parsing default
    
    * support parsing default
    
    * support parsing default

--- udao_trace_examples/spark_trace_eval.py
+++ udao_trace_examples/spark_trace_eval.py
@@ -1,7 +1,14 @@
+import glob
+import os.path
 from argparse import ArgumentParser
-from typing import Dict, List, Tuple
+from collections import defaultdict
+from typing import Any, Dict, List, Optional, Tuple
 
+import numpy as np
+
+from udao_spark.optimizer.utils import get_cloud_cost_add_io, get_cloud_cost_wo_io
 from udao_trace.collector.SparkCollector import SparkCollector
+from udao_trace.parser.spark_parser import parse_lqp_objectives
 from udao_trace.utils import BenchmarkType, ClusterName, JsonHandler
 from udao_trace_examples.params import get_collector_parser
 
@@ -49,6 +56,15 @@ def validate_and_parse_configurations(
     return header, configurations
 
 
+def get_mean_std_without_none(
+    raw_numbers: List[Optional[float]],
+) -> Tuple[float, float]:
+    ll = [x for x in raw_numbers if x is not None]
+    if len(ll) == 0:
+        return 0, 0
+    return np.mean(ll).astype(float), np.std(ll).astype(float)
+
+
 def get_eval_parser() -> ArgumentParser:
     parser = get_collector_parser()
     # fmt: off
@@ -58,6 +74,10 @@ def get_eval_parser() -> ArgumentParser:
                         help="Use fine-grained configurations")
     parser.add_argument("--n_reps", type=int, default=3,
                         help="Number of repetitions for each configuration")
+    parser.add_argument("--parse_only", action="store_true",
+                        help="Only parse configurations, do not start evaluations")
+    parser.add_argument("--force", action="store_true",
+                        help="Force parsing even if the file already exists")
     # fmt: on
 
     return parser
@@ -78,18 +98,100 @@ if __name__ == "__main__":
         debug=args.debug,
     )
 
-    header, configurations = validate_and_parse_configurations(
-        spark_collector=spark_collector,
-        config_header=args.configuration_header,
-        n_data_per_template=args.n_data_per_template,
-        fine_grained=args.fine_grained,
-    )
-
-    for i in range(args.n_reps):
-        print("------ Starting evaluation", i + 1)
-        spark_collector.start_eval(
-            eval_header=header,
-            configurations=configurations,
-            n_processes=args.n_processes,
-            cluster_cores=args.cluster_cores,
+    if args.default:
+        header = f"{spark_collector.header}/default_conf"
+        configuration = {
+            k.name: k.default for k in spark_collector.spark_conf.knob_list
+        }
+        configurations = []
+        for template in spark_collector.benchmark.templates:
+            for qid in range(1, args.n_data_per_template + 1):
+                configurations.append({(template, qid): configuration})
+    else:
+        header, configurations = validate_and_parse_configurations(
+            spark_collector=spark_collector,
+            config_header=args.configuration_header,
+            n_data_per_template=args.n_data_per_template,
+            fine_grained=args.fine_grained,
         )
+
+    if not args.parse_only:
+        print("------ Starting evaluations")
+        for i in range(args.n_reps):
+            print("------ Starting evaluation", i + 1)
+            spark_collector.start_eval(
+                eval_header=header,
+                configurations=configurations,
+                n_processes=args.n_processes,
+                cluster_cores=args.cluster_cores,
+            )
+        print("------ Finished all evaluations")
+
+    print("------ Start parsing")
+
+    file_name = f"{header}/evaluations.json"
+
+    if args.force:
+        print("------ Force parsing")
+    else:
+        if os.path.exists(file_name):
+            print("------ File already exists")
+            try:
+                j = JsonHandler.load_json(file_name)
+                print(j["agg_stats"])
+                exit(0)
+            except Exception as e:
+                print(f"------ File is corrupted {e}, force parsing")
+
+    stats_dict = {}
+    agg_stats_dict = {}
+    for conf in configurations:
+        (template, qid), configuration = list(conf.items())[0]
+        json_files = glob.glob(f"{header}/trace/*_{template}-{qid}_*.json")
+        print(f"------ Parsing {template}-{qid}, found {len(json_files)} files")
+        stats: Dict[str, List[Any]] = defaultdict(list)
+        for json_file in json_files:
+            try:
+                d = JsonHandler.load_json(json_file)
+            except Exception as e:
+                raise Exception(f"Failed to load {json_file} with error: {e}")
+            obj_dict = parse_lqp_objectives(d["Objectives"])
+            lat_s = obj_dict["latency_s"]
+            io_mb = obj_dict["io_mb"]
+            total_cores = int(configuration["spark.executor.cores"]) * (
+                int(configuration["spark.executor.instances"]) + 1
+            )
+            cost_wo_io = get_cloud_cost_wo_io(
+                lat=lat_s,
+                cores=int(configuration["spark.executor.cores"]),
+                mem=int(configuration["spark.executor.memory"].replace("g", "")),
+                nexec=int(configuration["spark.executor.instances"]),
+            )
+            cost_w_io = get_cloud_cost_add_io(
+                cost_wo_io=cost_wo_io,
+                io_mb=io_mb,
+            )
+            stats["json_trace"].append(json_file)
+            stats["latency_s"].append(lat_s)
+            stats["io_mb"].append(io_mb)
+            stats["cores"].append(total_cores)
+            stats["cost_wo_io"].append(cost_wo_io)
+            stats["cost_w_io"].append(cost_w_io)
+        stats_dict[(template, qid)] = stats
+        agg_stats_dict[(template, qid)] = {
+            "latency_s": get_mean_std_without_none(stats["latency_s"]),
+            "io_mb": get_mean_std_without_none(stats["io_mb"]),
+            "cores": get_mean_std_without_none(stats["cores"]),
+            "cost_wo_io": get_mean_std_without_none(stats["cost_wo_io"]),
+            "cost_w_io": get_mean_std_without_none(stats["cost_w_io"]),
+        }
+    JsonHandler.dump_to_file(
+        {
+            "stats": {f"{k[0]}-{k[1]}": v for k, v in stats_dict.items()},
+            "agg_stats": {f"{k[0]}-{k[1]}": v for k, v in agg_stats_dict.items()},
+        },
+        file_name,
+        indent=2,
+    )
+    j = JsonHandler.load_json(file_name)
+    print(j["agg_stats"])
