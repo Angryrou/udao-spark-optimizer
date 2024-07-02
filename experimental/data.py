d27ca837b25ce79ea3fa36790414ee80daea35a7
glachaud <guillaume.lachaud78@gmail.com>
2024-07-02 16:23:17 +0200

    Load query dataset as a Pandas DataFrame.
    The load_data function takes a folder name and a filename, and returns
    the query dataset (e.g., TPC-H or TPC-DS) as a Pandas DataFrame.

---
+++ experimental/data.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from udao_trace.utils.handler import ParquetHandler
+
+import pandas as pd
+
+
+def load_data(folder: Path, filename: str) -> pd.DataFrame:
+    """Load query dataset as a Pandas DataFrame
+
+    This function loads a dataset such as TPC-DS and returns it as a pandas DataFrame.
+    Args:
+        folder (Path): Location of the directory containing the dataset.
+        filename (str): Name of the file containing the dataset.
+
+    Returns:
+        pd.DataFrame: Query dataset.
+    """
+    df = ParquetHandler.load(folder, filename)
+    return df
+
+
+
